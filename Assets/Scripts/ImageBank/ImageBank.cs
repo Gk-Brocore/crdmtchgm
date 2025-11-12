04ee05ebbedb27e8dc9b7c09afb1190f21e09c92
gk <gk@no-bs.online>
2025-11-13 03:00:23 +0530

    feat(ImageBank): Added a ScriptableObject ImageBank.cs that maps ids → addressable references. folder(Data): Moved Grid data to Data/Grids

---
+++ Assets/Scripts/ImageBank/ImageBank.cs
@@ -0,0 +1,42 @@
+using Game.Addressable;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Game.ImageBank
+{
+    [CreateAssetMenu(fileName = "ImageBank", menuName = "Game/ImageBank")]
+    public class ImageBank : ScriptableObject
+    {
+        [Serializable]  
+        public class Entry
+        {
+            public string id;
+            public AssetReference spriteRef;
+        }
+
+        public List<Entry> entries = new List<Entry>();
+
+        public AssetReference GetSpriteReferenceById(string _id)
+        {
+            foreach (var entry in entries)
+            {
+                if (entry.id == _id)
+                {
+                    return entry.spriteRef;
+                }
+            }
+            return null;
+        }
+
+        public async Task<Sprite> GetSprite(string _id)
+        {
+            var _assetRef = GetSpriteReferenceById(_id);
+            return await AddressableManager.Instance.LoadImageAsync<Sprite>(_assetRef);
+        }
+
+    }
+}
